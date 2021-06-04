5695215ab030f32d6b639974c8806746e519a44b
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-06-04 09:47:18 +0530

    Update fuking.py

--- fuking.py
+++ fuking.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/theshashankk/spammer && cd spammer && python3 -m userbot")
+system ("git clone https://github.com/theshashankk/spammer.git && cd spammer && python3 -m userbot")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO YOUR FATHER")
